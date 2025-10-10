c2948f74f6a24c2e7aed85a7f7b878a3212ef48e
Ninjanarwhal41 <88503808+Ninjanarwhal41@users.noreply.github.com>
2025-10-10 08:58:00 -0600

    Update final.py
    added comments, grouped code together, and added a function for LCD_screen.

--- final.py
+++ final.py
@@ -1,32 +1,41 @@
 import RPi.GPIO as GPIO     ###various modules for speech-to-text, text-to-speech, lcd screen,and GPIO 
 import time
 import random
-import whisper
-import speech_recognition as sr
-import openai
-from gtts import gTTS
-from rpi_lcd import LCD
+import whisper #stt
+import speech_recognition as sr #stt
+import openai #stt
+from gtts import gTTS #tts
+from rpi_lcd import LCD #lcd screen
 from io import BytesIO
-from pygame import mixer
+from pygame import mixer #tts
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(26, GPIO.IN) #setting up microphone input on pin 26 
+lcd = LCD()
+lcd.clear()
+text_output = "hello world, hello world! hello world."
+
 
 model = whisper.load_model("base")
 recognizer = sr.Recognizer()
 
-GPIO.setup(26, GPIO.IN)
-
 AM = open("hate.txt", 'r')
 script = AM.read()
 
+def lcd_text(txt)
+    for i in range(len(txt)):
+        lcd.text(txt[i : i + 1])
+        time.sleep(0.1)
+    #goal is to create a scrolling effect, not sure if this will work. 
+    time.sleep(3) #stays for 3 seconds after 
+    lcd.clear() 
+
 def listen():
     while True:
-        for index, name in enumerate(sr.Microphone.list_microphone_names()):
-            print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
         try:
-            with sr.Microphone(device_index=0) as source:
-                print("Say something")
+            with sr.Microphone() as source:
+                print("Speaketh.")
                 audio = recognizer.listen(source)
 
             with open("temp.wav", "wb") as f:
@@ -36,6 +45,7 @@ def listen():
             result = model.transcribe("temp.wav")
             text = result["text"]
             print("You said:", text)
+            return text
         except Exception as e:
             print("Error", e)
 
@@ -46,6 +56,7 @@ def speak(txt):
     return mp3_fp
 
 listen()
+lcd_txt(script)
 """mixer.init()
 sound = speak(script)
 sound.seek(0)
@@ -54,17 +65,5 @@ mixer.music.play()
 
 time.sleep(60)
 #TTS speaking function above
+"""
 
-
-
-
-lcd = LCD()
-
-lcd.clear()
-text_output = "testing testing one two three"
-
-
-
-lcd.text(text_output, 1)
-time.sleep(5)
-lcd.clear() """
